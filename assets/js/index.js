d78b6289e320402917871850afc30cb6e9188215
Kahwasaurus <leahkahwadjian@gmail.com>
2018-04-12 01:45:38 -0400

    updated variable "canvas" to be a global var

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,10 @@
-
+const canvas = $('#pixelCanvas');
 
 $("input[type='submit']").on('click',function(event) {
 	event.preventDefault();
 	$('#pixelCanvas').empty();
 	const height = $('#inputHeight').val();
 	const width = $('#inputWidth').val(); 
-	const canvas = $('#pixelCanvas');
 	console.log($('#colorPicker').val());
 	makeGrid(height, width);
 
@@ -14,14 +13,14 @@ $("input[type='submit']").on('click',function(event) {
 function makeGrid(height, width) {
 	console.log(height, width);
 	for (let y=0; y<height; y++) {
-		$('#pixelCanvas').append($('<tr></tr>')); 
+		canvas.append($('<tr></tr>')); 
 		for (let x=0; x<width; x++) {
 			$('tr').last().append($('<td></td>'));
 		}
 	}
 }
 
-$('#pixelCanvas').on('mousedown mouseover', 'td', function(e) {
+canvas.on('mousedown mouseover', 'td', function(e) {
 	if (e.buttons===1) {
 	$(this).css('backgroundColor', $('#colorPicker').val());
 }
